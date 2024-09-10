41635ee1c351b19e34ab6c0e51d87229afb660a3
MAHDI BELABBAS <mahdibelabbas@gmail.com>
2024-09-10 13:10:03 -0400

    Music class test passed

--- classes/Music.js
+++ classes/Music.js
@@ -2,7 +2,24 @@
 const Media = require("./Media");
 // create your Music class:
 class Music extends Media {
+  constructor(title, genre, year, artist, length) {
+    super(title, genre, year);
+    this.artist = artist;
+    this.length = length;
+  }
+  summary() {
+    return `Title: ${this.title}, Artist: ${this.artist}, Year: ${this.year}, Genre: ${this.genre}, Length: ${this.length} seconds`;
+  }
+  static shortestAlbum(music) {
+   let Shortestmusic=music[0];
+   for(let i=0;i< music.length;i++){
+     if(music[i].length< Shortestmusic.length){
+       Shortestmusic= music[i];
+     }
+   }
+   return Shortestmusic;
 
+  }
 }
 // don't change below
 module.exports = Music;

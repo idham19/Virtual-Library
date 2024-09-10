cf3ea88aba8f34be35d042de9c331074211beba8
MAHDI BELABBAS <mahdibelabbas@gmail.com>
2024-09-10 12:50:14 -0400

    Movie class test passed

--- classes/Movie.js
+++ classes/Movie.js
@@ -2,23 +2,23 @@
 const Media = require("./Media");
 // create your Movie class:
 class Movie extends Media {
-  constructor(title, year, genre, director, duration, average) {
+  constructor(title, year, genre, director, duration, rating) {
     super(title, year, genre);
     this.director = director;
     this.duration = duration;
-    this.average = average;
-    // if (1 <= rating <= 5) {
-    //   this.rating = rating;
-    // } else {
-    //   throw new Error("Rating must be between 1 and 5.");
-    // }
+    if (rating>=1 && rating <= 5) {
+      this.rating = rating;
+    } else {
+      throw new Error("Rating must be between 1 and 5.");
+    }
   }
 
   summary() {
-    return `Title: ${this.title}, Director: ${this.director}, Year: ${this.year}, Genre: ${this.genre}, Duration: ${this.duration}, Rating: ${this.rating}`;
+    return `Title: ${this.title}, Director: ${this.director}, Year: ${this.year}, Genre: ${this.genre}, Rating: ${this.rating}`;
+    //Title: <TITTLE>, Director: <DIRECTOR>, Year: <YEAR>, Genre: <GENRE>, Duration: <DURATION>, Rating: <RATING>
   }
   static longestMovie(movie) {
-      longestMovieDuration = movie[0]; 
+      let longestMovieDuration = movie[0]; 
     for (let i = 0; i < movie.length; i++) {
       if (movie[i].duration > longestMovieDuration.duration) {
         longestMovieDuration=movie[i];
